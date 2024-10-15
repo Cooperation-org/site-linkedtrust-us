1b580db4cac9dcef41486db5fc6e2133b63af896
Emos21 <emosmwangi@gmail.com>
2024-10-16 00:17:29 +0300

    added 3 more pages

--- New_website_code/website/urls.py
+++ New_website_code/website/urls.py
@@ -4,4 +4,9 @@ from .views import *
 urlpatterns = [
     path('', home_view, name='home'),
     path('about', about_view, name='about'),
+    path('services', services_view, name='services'),
+    path('page4', page4_view, name='page4'),
+    path('getstarted', getstarted_view, name='getstarted'),
 ]
+
+# 
--- New_website_code/website/views.py
+++ New_website_code/website/views.py
@@ -6,3 +6,14 @@ def home_view(request):
 
 def about_view(request):
     return render(request, 'about.html')
+
+def services_view(request):
+    return render(request, 'services.html')
+
+def page4_view(request):
+    return render(request, 'page4.html')
+
+def getstarted_view(request):
+    return render(request, 'getstarted.html')
+
+# 
