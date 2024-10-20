fc68d0805a3e3c08ce1e6ce40214d989b003f204
Emos21 <emosmwangi@gmail.com>
2024-10-20 16:19:15 +0300

    Updated the navbar, added vide background removed the about section in the home page, added more padding on the whitepaper section, added a GetStarted section on the home page.

--- New_website_code/linkedtrust/settings.py
+++ New_website_code/linkedtrust/settings.py
@@ -25,6 +25,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'website',
+    'livereload',
 ]
 
 MIDDLEWARE = [
@@ -35,6 +36,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'livereload.middleware.LiveReloadScript',
 ]
 
 ROOT_URLCONF = 'linkedtrust.urls'
