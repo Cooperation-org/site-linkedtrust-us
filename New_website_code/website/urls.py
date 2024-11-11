b68d1555d261f72fc934d1bcc964f23151957440
gv <gv@mes-MacBook-Pro.local>
2024-11-11 14:39:29 -0700

    start filling in the blanks!

--- New_website_code/website/urls.py
+++ New_website_code/website/urls.py
@@ -7,4 +7,9 @@ urlpatterns = [
     path('services', services_view, name='services'),
     path('getstarted', getstarted_view, name='getstarted'),
     path('contact', contact_view, name='contact'),
+
+
+    # TBD views
+    path('mission', empty_view, name='mission'),
+    path('press', empty_view, name='press'),
 ]
